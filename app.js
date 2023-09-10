2153acb75c1a77f521f012de29799f279bfab38f
chima <emmycruz12@gmail.com>
2023-09-10 03:10:20 -0700

    fixe qurey

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 
+// middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
+  const { slack_name, track } = req.query;
+
   const currentTime = new Date();
   utc_time = Math.floor(currentTime.getTime() / 1000);
   const dayOfWeek = currentTime.getDay();
@@ -16,6 +21,22 @@ app.get("/", (req, res) => {
     "Friday",
     "Saturday",
   ];
+  if (slack_name && track) {
+    return res.status(200).json({
+      status_code: 200,
+      slack_name: "emmanuel chima",
+      track: "Backend",
+    });
+  }
+
+  if (slack_name) {
+    return res
+      .status(200)
+      .json({ status_code: 200, slack_name: "emmanuel chima" });
+  }
+  if (track) {
+    return res.status(200).json({ status_code: 200, track: "Backend" });
+  }
 
   res.status(200).json({
     status_code: 200,
